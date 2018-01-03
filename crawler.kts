f069eb06aba206ccb5ce609b0c601561e6428e9f
ch4inl3ss <ch4inl3ss@gmail.com>
2018-01-03 22:37:16 +0100

    script angelegt

---
+++ crawler.kts
@@ -0,0 +1,10 @@
+//textmate kotlin-bundle
+//https://github.com/sargunv/kotlin-textmate-bundle
+//brew install kotlin...
+//normally: kotlinc -script <file.kts> <arguments> -- done by bundle
+
+
+//.kts-file
+//--> no class etc.
+
+print("Hello World")
